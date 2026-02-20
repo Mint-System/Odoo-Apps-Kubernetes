f4d8622a9010bf167d728ba35b88a53cb7c14e58
Janik von Rotz <login@janikvonrotz.ch>
2026-02-20 16:04:26 +0100

    feat(helm): update product template and partner models for consulting partner management
    - Added partner_ids many2many field to product.template for consulting partner assignment
    - Changed helm_product_ids from product.product to product.template in res.partner
    - Updated demo data to use product.template records instead of product.product
    - Added consulting partners section in product template form view with list widget
    - Fixed demo partner record to reference correct product template IDs

--- helm/models/product_template.py
+++ helm/models/product_template.py
@@ -5,3 +5,4 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     chart_id = fields.Many2one("helm.chart")
+    partner_ids = fields.Many2many("res.partner", string="Consulting Partners")
--- helm/models/res_partner.py
+++ helm/models/res_partner.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class Partner(models.Model):
     _inherit = "res.partner"
 
-    helm_product_ids = fields.Many2many("product.product")
+    helm_product_ids = fields.Many2many("product.template")
